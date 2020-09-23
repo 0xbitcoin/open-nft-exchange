de877aa93b0538524ba39b57b03db2b26774f68e
andy <andy@jonezie.com>
2020-09-22 21:55:09 -0400

    interface work s

--- contracts/OpenNFTExchange.sol
+++ contracts/OpenNFTExchange.sol
@@ -125,23 +125,15 @@ interface ERC165 {
    function supportsInterface(bytes4 interfaceID) external view returns (bool);
 }*/
 
-interface iERC721Receiver {
-   /// @notice Handle the receipt of an NFT
-   /// @dev The ERC721 smart contract calls this function on the
-   /// recipient after a `transfer`. This function MAY throw to revert and reject the transfer. Return
-   /// of other than the magic value MUST result in the transaction being reverted.
-   /// @notice The contract address is always the message sender.
-   /// @param _operator The address which called `safeTransferFrom` function
-   /// @param _from The address which previously owned the token
-   /// @param _tokenId The NFT identifier which is being transferred
-   /// @param _data Additional data with no specified format
-   /// @return `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`
-   /// unless throwing
-   function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes calldata _data) external returns(bytes4);
+ 
+
+
+contract ERC721Receiver {
+
+    function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes memory _data) public returns(bytes4);
 }
 
-
-contract OpenNFTExchange   {
+contract OpenNFTExchange is ERC721Receiver {
 
     using SafeMath for uint;
 
